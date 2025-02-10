1f1b9eed05b3930fc45c3960f143a988cafeac7d
Adham-m7md <adhammohamed.mb@gmail.com>
2025-02-10 21:16:54 +0200

    page view item 1

--- lib/features/on_boarding/presentation/widgets/onboarding_page_view.dart
+++ lib/features/on_boarding/presentation/widgets/onboarding_page_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fresh_fruit/core/utils/svg_images.dart';
+import 'package:fresh_fruit/features/on_boarding/presentation/widgets/page_view_item.dart';
 
 class OnboardingPageView extends StatelessWidget {
   const OnboardingPageView({super.key});
@@ -8,8 +9,22 @@ class OnboardingPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: [
-        SvgPicture.asset(Assets.imagesBackgroundOnboarding1),
+      children: const [
+        PageViewItem(
+          image: 'assets/images/onboarding1.png',
+          backGroundImage: Assets.imagesBackgroundOnboarding1,
+          description:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          titel: Row(
+            children: [
+              Text('مرحبًا بك في'),
+              Text('Fruit'),
+              Text(
+                'HUB',
+              )
+            ],
+          ),
+        )
       ],
     );
   }
---
+++ lib/features/on_boarding/presentation/widgets/page_view_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fresh_fruit/core/utils/svg_images.dart';
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem(
+      {super.key,
+      required this.image,
+      required this.backGroundImage,
+      required this.description,
+      required this.titel});
+  final String image, backGroundImage;
+  final String description;
+  final Widget titel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: SvgPicture.asset(
+                    backGroundImage,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Positioned(
+                  bottom: -80,
+                  right: 0,
+                  left: 0,
+                  child: Image.asset(image),
+                ),
+                const Text('تخط')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
