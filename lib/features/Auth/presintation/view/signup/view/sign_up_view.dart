b295bc918c5d3cc23f9106f7b01031ce0dce254e
Adham-m7md <adhammohamed.mb@gmail.com>
2025-02-20 03:27:26 +0200

    added email & pass , name feild to sign up view

--- lib/features/Auth/presintation/view/signup/view/sign_up_view.dart
+++ lib/features/Auth/presintation/view/signup/view/sign_up_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fresh_fruit/core/helper/widgets/custom_app_bar.dart';
 import 'package:fresh_fruit/core/utils/app_colors.dart';
-import 'package:fresh_fruit/core/utils/app_text_styles.dart';
+
 import 'package:fresh_fruit/features/Auth/presintation/view/signup/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -9,20 +10,9 @@ class SignUpView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
+      backgroundColor: AppColors.kWiteColor,
+      appBar: buildAppBar(context, titel: 'حساب جديد'),
       body: const SignUpViewBody(),
     );
   }
-}
-
-AppBar buildAppBar() {
-  return AppBar(
-    backgroundColor: AppColors.kWiteColor,
-    centerTitle: true,
-    title: const Text(
-      'حساب جديد',
-      style: TextStyles.bold16,
-    ),
-    leading: Icon(Icons.arrow_back_ios_new_outlined),
-  );
 }
--- lib/features/Auth/presintation/view/signup/widgets/sign_up_view_body.dart
+++ lib/features/Auth/presintation/view/signup/widgets/sign_up_view_body.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:fresh_fruit/core/constants.dart';
+import 'package:fresh_fruit/core/helper/widgets/custom_text_form_feild.dart';
+import 'package:fresh_fruit/core/utils/app_colors.dart';
+import 'package:fresh_fruit/core/utils/app_directions.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-      child: SingleChildScrollView(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: context.screenHeight * 0.03),
+            const CustomTextFormFeild(
+              hintText: 'الاسم كامل',
+              keyBoardType: TextInputType.text,
+            ),
+            SizedBox(height: context.screenHeight * 0.02),
+            const CustomTextFormFeild(
+              hintText: 'البريد الإلكتروني',
+              keyBoardType: TextInputType.text,
+            ),
+            SizedBox(height: context.screenHeight * 0.02),
+            CustomTextFormFeild(
+              hintText: 'كلمة المرور',
+              keyBoardType: TextInputType.text,
+              suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.remove_red_eye,
+                    color: AppColors.kGrayColor,
+                  )),
+            ),
+            SizedBox(height: context.screenHeight * 0.03),
+          ],
+        ),
+      ),
     );
   }
 }
