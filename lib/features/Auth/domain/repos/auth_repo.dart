b55af79232bb455983e60e50d69e27d5b270a3c0
Adham-m7md <adhammohamed.mb@gmail.com>
2025-02-22 00:45:43 +0200

    .

--- lib/features/Auth/domain/repos/auth_repo.dart
+++ lib/features/Auth/domain/repos/auth_repo.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fresh_fruit/core/errors/failures.dart';
 import 'package:fresh_fruit/features/Auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failures, UserEntity>> createUserWithEmailAndPassword(
       String email, String password, String name);
 }
